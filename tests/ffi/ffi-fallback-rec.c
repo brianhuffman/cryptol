37c430460797da3fa624c316959bc6b2a9546c38
Brian Huffman <huffman.brian.c@gmail.com>
2025-03-13 18:17:14 -0700

    Remove ffi and suiteb regression tests.

--- tests/ffi/ffi-fallback-rec.c
+++
@@ -1,5 +0,0 @@
-#include <stdint.h>
-
-uint8_t f(uint8_t x) {
-    return x;
-}
--- tests/ffi/ffi-fallback.c
+++
@@ -1,9 +0,0 @@
-#include <stdint.h>
-
-uint8_t foreignAndCryptol(uint8_t x) {
-    return x + 1;
-}
-
-uint8_t foreignAndNoCryptol(uint8_t x) {
-    return x + 2;
-}
